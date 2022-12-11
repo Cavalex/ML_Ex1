54f638cacbb99bfce73de48d2138b2ae93f76a0f
Cavalex <mateusmpta@gmail.com>
2022-12-11 04:26:41 +0100

    finished nb

--- ex2/nb.py
+++ ex2/nb.py
@@ -2,66 +2,119 @@ import numpy as np
 
 class GaussianNaiveBayes:
 
-    def fit(self, X, y):        
+    """
+    P(A | B) = P(B | A) * P(A) / P(B)
+    =
+    P(y | X) = P(X | y) * P(y) / P(X)
+
+    P(X) -> MARGINAL LIKELYHOOD
+    P(y) -> PRIOR -> FREQUENCY OF EACH CLASS
+    P(X / xi | y) -> LIKELIHOOD OR CLASS CONDITIONAL PROBABILITY-> MODEL WITH GAUSSIAN
+    P(y | X) -> POSTERIOR
+
+    X -> FEATURES
+    y -> LABELS
+
+    TRAINING: 
+        MEAN, VARIANCE AND PRIOR OF EACH CLASS
+
+    PREDICTIONS: 
+        POSTERIOR OF EACH CLASS
+            CALCULATE THE LIKELIHOOD
+        CHOOSE CLASS WITH HIGHEST POSTERIOR PROBABILITY
+    """
+
+    # training the model, i.e. calculating everything necessary to get the posterior on the predict function.
+    def fit(self, X, y):
+        self.labels = y
         self.unique_labels = np.unique(y)
 
-        self.params = []
+        # we need the means and variances to calculate the likelihood
+        self.means = []
+        self.vars = []
+        self.priors = []
+
+        # for every label, calculate the mean and variance of all features
         for label in self.unique_labels:
-            label_features = X[y == label]
-            self.params.append([(col.mean(), col.var()) for col in label_features.T])
+            label_features = X[y == label] # remove all other classes except the one selected
+            self.means.append([col.mean() for col in label_features.T])
+            self.vars.append([col.var() for col in label_features.T])
+            self.priors.append(label_features.shape[0] / X.shape[0]) # number of samples of this class divided by total number of samples
 
+        #print("\nX:", X)
+        #print("\nvars:", self.vars)
+        #print("\nmeans:", self.means)
+        #print("\npriors:", self.priors)
 
-    """ def fit(self, X, y):
-        # get number of samples (rows) and features (columns)
-        self.n_samples, self.n_features = X.shape
-        # get number of uniques classes
-        self.n_classes = len(np.unique(y))
-        
-        # create three zero-matrices to store summary stats & prior
-        self.mean = np.zeros((self.n_classes, self.n_features))
-        self.variance = np.zeros((self.n_classes, self.n_features))
-        self.priors = np.zeros(self.n_classes)
+    # predicting the target values given X features. will basically calculate the posterior of each class given the training data
+    def predict(self, X):
 
-        for c in range(self.n_classes):
-            # create a subset of data for the specific class 'c'
-            X_c = X[y == c]
-            
-            # calculate statistics and update zero-matrices, rows=classes, cols=features
-            self.mean[c, :] = np.mean(X_c, axis=0)
-            self.variance[c, :] = np.var(X_c, axis=0)
-            self.priors[c] = X_c.shape[0] / self.n_samples """
+        num_samples, num_features = X.shape
 
+        predictions = []
 
+        # we now calculate the posterior for each label
+        # we will use the formula on notes/posterior_formula.png
+        for x in X:
+            posteriors = [] # we want to select the most likely one according to each label/class
+            for i, c in enumerate(self.unique_labels):
+                likelihoods = []
+                for j, value in enumerate(x):
 
+                    #print("\nx:", x)
+                    #print("\nvars:", self.vars)
+                    #print("\nmeans:", self.means)
+                    #print("\npriors:", self.priors)
 
+                    #print("\nfeature:", x)
+                    #print("\nmeans:", self.means[i])
+                    #print("\nvars:", self.vars[i])
+                    #break
 
+                    # now we calculate the likelihood of each feature and append it to then sum everything together
+                    # this is like P(xi | y)
+                    # so we need to sum to get P(X | y)
+                    likelihood = np.log(self.likelihood(value, self.means[i][j], self.vars[i][j]))
+                    likelihoods.append(likelihood)
 
-"""     
-    features: np.ndarray # columns of the input
-    labels: np.ndarray # output
+                posterior = np.sum(likelihoods) + self.priors[i]
+                posteriors.append(posterior)
 
-    # Fits the model
-    def fit(self) -> None:        
-        self.unique_labels = np.unique(self.labels)
+            #print(posteriors)
+            predictions.append(np.argmax(posteriors))
 
-        self.params = []
-        for label in self.unique_labels:
-            label_features = self.features[self.labels == label]
-            self.params.append([(col.mean(), col.var()) for col in label_features.T])
+        return np.array(predictions) # it must be a np array to work with scikit later on
 
+    # calculates the gaussian likelihood of the data with the given mean and variance.
+    def likelihood(self, x, mean, var):
 
-    # Performs the inference using bayes theorem: P(A|B) = P(B|A) * P(A) / P(B)
-    def predict(self, features: np.ndarray):# -> np.ndarray:
-        num_samples, _ = features.shape
-        predictions = np.empty(num_samples) # initializes it
+        # NOTE: Added in denominator to prevent division by zero
+        eps = 1e-4
 
-        for feature in features:
-            posteriors = []
-            for feature in features:
-                posteriors = []
-                for label_idx, label in enumerate(self.unique_labels):
+        coeff = 1 / np.sqrt(2 * np.pi * var + eps)
+        exponent = np.exp(-((x - mean) ** 2 / (2 * var + eps)))
 
-                    prior = (self.labels==label).mean()
+        return coeff * exponent
 
+# Testing
+if __name__ == "__main__":
+    # Imports
+    from sklearn.model_selection import train_test_split
+    from sklearn import datasets
 
- """
+    def accuracy(y_true, y_pred):
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
+
+    X, y = datasets.make_classification(
+        n_samples=1000, n_features=10, n_classes=2, random_state=123
+    )
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=123
+    )
+
+    nb = GaussianNaiveBayes()
+    nb.fit(X_train, y_train)
+    predictions = nb.predict(X_test)
+
+    print("Naive Bayes classification accuracy", accuracy(y_test, predictions))
